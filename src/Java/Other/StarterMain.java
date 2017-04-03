d69f8447d7b9c336559cce667b1b2b6e0d808c19
master <sabermeyt@gmail.com>
2017-04-03 09:54:23 +0300

    v2.1 При вызове метода start() от экземпляра StarterMain корректно вызываются Р_машина и отладчик.

--- src/Java/Other/StarterMain.java
+++ src/Java/Other/StarterMain.java
@@ -1,10 +1,7 @@
 package Other;
 
 import XmlReader.AlgorithmReaderNew;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
+import gui.window.makery.address.model.DebuggerRunner;
 
 /**
  * Created by master on 20.03.2017.
@@ -23,26 +20,33 @@ public class StarterMain {
     boolean debugType;
     String tape;
 
-    public StarterMain(String filepath, boolean debugType, String tape) throws ParserConfigurationException, SAXException, IOException, InterruptedException {
+    public StarterMain(String filepath, boolean debugType, String tape) throws Exception {
         this.filepath = filepath;
         this.debugType = debugType;
         this.tape = tape;
         start();
     }
 
-    public String start() throws IOException, SAXException, ParserConfigurationException, InterruptedException {
-        AlgorithmReaderNew algorithmReader = new AlgorithmReaderNew(filepath);
-        algorithmReader.readMemories();
-        algorithmReader.readAlgorithm();
-        Storage storage = new Storage(algorithmReader.arms,algorithmReader.memoryHashMap,algorithmReader.alphabetHashMap);
-        AllStorage allStorage = new AllStorage(storage,new Tape(tape));
-        R_machine r_machine = new R_machine(allStorage);
-        r_machine.simpleRun();
+    public String start() throws Exception {
+        if(!this.debugType) {
+            AlgorithmReaderNew algorithmReader = new AlgorithmReaderNew(filepath);
+            algorithmReader.readMemories();
+            algorithmReader.readAlgorithm();
+            Storage storage = new Storage(algorithmReader.arms, algorithmReader.memoryHashMap, algorithmReader.alphabetHashMap);
+            AllStorage allStorage = new AllStorage(storage, new Tape(tape));
+            R_machine r_machine = new R_machine(allStorage);
+            r_machine.simpleRun();
+            return "0";
+        }else{
+            DebuggerRunner debuggerRunner = new DebuggerRunner(this.filepath,this.tape);
+            debuggerRunner.start();
+        }
         return "0";
     }
 
-    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException, InterruptedException {
+    public static void main(String[] args) throws Exception {
         StarterMain starterMain = new StarterMain("templateStrorageTest.xml",false,"perfectapple#");
+        StarterMain starterMain2 = new StarterMain("templateStrorageTest.xml",true,"perfectapple#");
     }
 
 }
--- src/Java/gui/window/makery/address/model/DebuggerRunner.java
+++ src/Java/gui/window/makery/address/model/DebuggerRunner.java
@@ -8,11 +8,19 @@ import XmlReader.AlgorithmReaderNew;
  * Created by Admin on 28.01.2017.
  */
 public class DebuggerRunner {
-	public static void main (String[] args) throws Exception{
-		AlgorithmReaderNew algorithmReader = new AlgorithmReaderNew("templateStrorageTest.xml");
+	String filepath;
+	String tape;
+
+	public DebuggerRunner(String filepath, String tape) {
+		this.filepath = filepath;
+		this.tape = tape;
+	}
+
+	public void start () throws Exception{
+		AlgorithmReaderNew algorithmReader = new AlgorithmReaderNew(filepath);
 		algorithmReader.readMemories();
 		algorithmReader.readAlgorithm();
-		Tape tape = new Tape("perfectapple#");
+		Tape tape = new Tape(this.tape);
 		Storage storage = new Storage(algorithmReader.arms,algorithmReader.memoryHashMap,algorithmReader.alphabetHashMap);
 		AllStorage allStorage = new AllStorage(storage,tape);
 		WorkExchange workExchange = new WorkExchange();
